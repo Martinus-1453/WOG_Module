5b9e79f5b203c67bdf65e40963921ffaeb079d4f
greeboCherry <greebo4pl@gmail.com>
2022-11-27 12:35:01 +0100

    Add printing of "WoG v 0.1 - %Y-%m-%d %H:%M:%S in the top left corner

--- src/wog/client/ClientSqMain.cpp
+++ src/wog/client/ClientSqMain.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "function/ClientFunctions.h"
 #include "ClientInit.h"
+#include "WogConfig.h"
+#include "class/Draw.h"
 
 using namespace SqModule;
 
@@ -16,6 +18,12 @@ extern "C" SQRESULT SQRAT_API sqmodule_load(HSQUIRRELVM vm, HSQAPI api)
 
 	// Test getWorld function + print
 	SHARED_FUNCTIONS->print(CLIENT_FUNCTIONS->getWorld());
+
+	// Draw module version and build time in the corner
+	const std::string corner_message = "WoG v" + std::to_string(wog_module_VERSION_MAJOR) + "." +
+									   std::to_string(wog_module_VERSION_MINOR) + " - " + wog_module_COMPILE_TIME_str;
+	const auto draw = new nonut::g2o::Draw(0, 0, corner_message); // TODO: consider having unique_ptr in some singleton to hold it?
+	draw->visible = true;
 
 	return SQ_OK;
 }
