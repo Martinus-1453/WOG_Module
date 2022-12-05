31ab3a3a065105e1d66773003bbc0295ea1a8c68
greeboCherry <greebo4pl@gmail.com>
2022-12-05 20:16:47 +0100

    Added SeverConstants and finished SharedConstants

---
+++ src/nonut/g2o/server/constant/ServerConstants.h
@@ -0,0 +1,53 @@
+#ifndef NONUT_G2O_SERVER_CONSTANT_SEVER_CONSTANTS_H
+#define NONUT_G2O_SERVER_CONSTANT_SEVER_CONSTANTS_H
+
+// AntyCheat
+
+/// @brief Represents speed hack state.
+static inline Int AC_SPEED_HACK = 0;
+
+// Network
+
+/// @brief disconnected player state.
+static inline Int DISCONNECTED = 0;
+/// @brief lost connection player state.
+static inline Int LOST_CONNECTION = 0;
+/// @brief crash player state.
+static inline Int HAS_CRASHED = 0;
+
+// Talent
+
+/// @brief Represents npc sneak talent.
+static inline Int TALENT_SNEAK = 0;
+/// @brief Represents npc picklock talent.
+static inline Int TALENT_PICK_LOCKS = 0;
+/// @brief Represents npc pickpocket talent.
+static inline Int TALENT_PICKPOCKET = 0;
+/// @brief Represents npc runes creation talent.
+static inline Int TALENT_RUNES = 0;
+/// @brief Represents npc potion creation talent.
+static inline Int TALENT_ALCHEMY = 0;
+/// @brief Represents npc smith talent.
+static inline Int TALENT_SMITH = 0;
+/// @brief Represents npc trophy gathering talent.
+static inline Int TALENT_THROPHY = 0;
+/// @brief Represents npc acrobatic talent.
+static inline Int TALENT_ACROBATIC = 0;
+/// @brief Represents npc health regeneration talent.
+static inline Int TALENT_REGENERATE = 0;
+/// @brief Represents npc firemaster talent (unused by the game).
+static inline Int TALENT_FIREMASTER = 0;
+/// @brief Represents npc old pickpocket talent (unused by the game).
+static inline Int TALENT_PICKPOCKET_UNUSED = 0;
+/// @brief Represents npc talent A (unused by the game).
+static inline Int TALENT_A = 0;
+/// @brief Represents npc talent B (unused by the game).
+static inline Int TALENT_B = 0;
+/// @brief Represents npc talent C (unused by the game).
+static inline Int TALENT_C = 0;
+/// @brief Represents npc talent D (unused by the game).
+static inline Int TALENT_D = 0;
+/// @brief Represents npc talent E (unused by the game).
+static inline Int TALENT_E = 0;
+
+#endif // NONUT_G2O_SERVER_CONSTANT_SEVER_CONSTANTS_H
--- src/nonut/g2o/shared/constant/SharedConstants.h
+++ src/nonut/g2o/shared/constant/SharedConstants.h
@@ -1,5 +1,5 @@
-#ifndef NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS_H
-#define NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS_H
+#ifndef NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS
+#define NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS
 #include "pch.h"
 
 namespace nonut::g2o
@@ -9,11 +9,79 @@ namespace nonut::g2o
 	public:
 		static void init();
 
+		// constants Reliability
+
 		static inline Int UNRELIABLE = 0;
 		static inline Int UNRELIABLE_SEQUENCED = 0;
 		static inline Int RELIABLE = 0;
 		static inline Int RELIABLE_ORDERED = 0;
 		static inline Int RELIABLE_SEQUENCED = 0;
+
+		// constants Damage
+
+		/// @brief Represents unknown damage type.
+		static inline Int DAMAGE_UNKNOWN = 0;
+		/// @brief Represents barrier damage type.
+		static inline Int DAMAGE_BARRIER = 0;
+		/// @brief Represents blunt (weapon) damage type.
+		static inline Int DAMAGE_BLUNT = 0;
+		/// @brief Represents edge/sharp (weapon) damage type.
+		static inline Int DAMAGE_EDGE = 0;
+		/// @brief Represents fire damage type.
+		static inline Int DAMAGE_FIRE = 0;
+		/// @brief Represents fly damage type.
+		static inline Int DAMAGE_FLY = 0;
+		/// @brief Represents magic damage type.
+		static inline Int DAMAGE_MAGIC = 0;
+		/// @brief Represents point (weapon) damage type.
+		static inline Int DAMAGE_POINT = 0;
+		/// @brief Represents fall damage type.
+		static inline Int DAMAGE_FALL = 0;
+
+		// constants General
+
+		/// @brief true if parsed script is on client-side, otherwise false.
+		static inline Int CLIENT_SIDE = 0;
+		/// @brief true if parsed script is on server-side, otherwise false.
+		static inline Int SERVER_SIDE = 0;
+
+		// constants Hand
+
+		/// @brief Represents npc left hand slot id.
+		static inline Int HAND_LEFT = 0;
+		/// @brief Represents npc right hand slot id.
+		static inline Int HAND_RIGHT = 0;
+
+		// constants Skill weapon
+		/// @brief Represents npc one handed weapon skill.
+		static inline Int WEAPON_1H = 0;
+		/// @brief Represents npc two handed weapon skill.
+		static inline Int WEAPON_2H = 0;
+		/// @brief Represents npc bow weapon skill.
+		static inline Int WEAPON_BOW = 0;
+		/// @brief Represents npc crossbow weapon skill.
+		static inline Int WEAPON_CBOW = 0;
+
+		// constants Weapon mode
+
+		/// @brief Represents npc none weapon mode.
+		static inline Int WEAPONMODE_NONE = 0;
+		/// @brief Represents npc fist weapon mode.
+		static inline Int WEAPONMODE_FIST = 0;
+		/// @brief Represents npc dagger weapon mode.
+		static inline Int WEAPONMODE_DAG = 0;
+		/// @brief Represents npc one handed weapon mode (melee weapon).
+		static inline Int WEAPONMODE_1HS = 0;
+		/// @brief Represents npc two handed weapon mode (melee weapon).
+		static inline Int WEAPONMODE_2HS = 0;
+		/// @brief Represents npc bow weapon mode (ranged weapon).
+		static inline Int WEAPONMODE_BOW = 0;
+		/// @brief Represents npc crossbow weapon mode (ranged weapon).
+		static inline Int WEAPONMODE_CBOW = 0;
+		/// @brief Represents npc magic weapon mode (spell).
+		static inline Int WEAPONMODE_MAG = 0;
+		/// @brief Represents npc maximum weapon mode (not actuall weapon mode, weapon modes count).
+		static inline Int WEAPONMODE_MAX = 0;
 	};
 }
-#endif // NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS_H
+#endif // NONUT_G2O_SHARED_CONSTANT_SHARED_CONSTANTS
